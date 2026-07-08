0476f6aead3875f3bfb1f4879cbfde943dae7dcb
HARIKARTHICK P <260403367+hackdesire370-creator@users.noreply.github.com>
2026-07-08 21:42:03 +0530

    Clean up test files for pristine new repo

--- test.js
+++
@@ -1,5 +0,0 @@
-import db from './server/db/database.js';
-import { createVault } from './server/controllers/vaultController.js';
-const req = { body: { category: 'Birthday', title: 'Test', wish: 'Happy BDay' } };
-const res = { status: (c) => ({ json: (d) => console.log(c, d) }) };
-createVault(req, res).then(() => process.exit(0)).catch(console.error);
